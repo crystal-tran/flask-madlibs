1ea4bf972d3a32515132a2a42a6ea9da5ad35cdc
Nicholas <nick.orsi1992@gmail.com>
2023-11-29 17:27:01 -0800

    Added TODO notes from code review.

--- app.py
+++ app.py
@@ -7,23 +7,26 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
-
+# TODO: Should rename the route to homepage, not madlibs, so should be "/"
 @app.get('/madlibs')
 def show_form():
     """Show Madlibs form"""
     # questions = silly_story.prompts
     return render_template(
         "questions.html",
-        prompts = silly_story.prompts
+        prompts=silly_story.prompts
 )
 
 @app.get('/results')
 def show_story():
     """Generate answers dictionary and show Madlibs result"""
+    # TODO: Remove line 24, 32-39, redundant. Only need to pass in request.args,
+    # this is already a dict like object (polymorphic to dict) made up of the
+    # query items
     answers = get_answers()
     return render_template(
         "results.html",
-        story = silly_story.get_result_text(answers)
+        story=silly_story.get_result_text(answers)
     )
 
 
