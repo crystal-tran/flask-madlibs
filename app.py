2e64acef282ef7a96cac9eb140c2dd5ff6611412
Nicholas <nick.orsi1992@gmail.com>
2023-11-29 16:52:35 -0800

    Add functionality to app.py to display the madlib and modified questions.htmla and results.html with jinja syntax to auto update.

--- app.py
+++ app.py
@@ -9,11 +9,30 @@ app.config['SECRET_KEY'] = "secret"
 debug = DebugToolbarExtension(app)
 
 @app.get('/madlibs')
-def form():
+def show_form():
     """Show Madlibs form"""
-    questions = silly_story.prompts
-    print("questions:", questions)
+    # questions = silly_story.prompts
     return render_template(
-        "questions.html"
+        "questions.html",
+        prompts = silly_story.prompts
 )
 
+@app.get('/results')
+def show_story():
+    """Generate answers dictionary and show Madlibs result"""
+    answers = get_answers()
+    return render_template(
+        "results.html",
+        story = silly_story.get_result_text(answers)
+    )
+
+
+
+def get_answers():
+    """Makes and returns a dictionary made up of key/value pairs from the
+    madlib form"""
+    prompts = silly_story.prompts
+    answers = {}
+    for prompt in prompts:
+        answers[prompt] = request.args.get(prompt)
+    return answers
